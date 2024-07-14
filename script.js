1d05617d247b13133a3df01f0b6f4ee89703c2b9
PappukarLibrary <158065740+PappukarLibrary@users.noreply.github.com>
2024-07-14 11:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function fibonacci(num) {
+	if(num == 1) return 0;
+	if(num == 2) return 1;
 	
  let num1 = 0;
 	let num2 = 1 ;
 	let i=0;
 	let sum =0;
-	for(let i=0;i<num;i++){
+	for(let i=2;i<=num;i++){
 		sum = num1+num2;
 		num1 = num2;
 		num2=sum;
 		
 	}
-	console.log(sum);
+	return num2;
 }
 
 module.exports = fibonacci;
